06ed1e1009e69eb11857b39243471754d174ca89
Rahma Maulina <92282754+rahmamln@users.noreply.github.com>
2022-04-24 01:37:20 +0700

    Delete models directory

--- models/Dosen_model.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Dosen_model extends CI_Model{
-    public $id, $nama, $gender, $tmp_lahir, $tgl_lahir, $nidn, $pendidikan;
-}
-?>
--- models/Matakuliah_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Matakuliah_model extends CI_Model{
-    public $id;
-    public $nama;
-    public $sks;
-    public $kode;
-}
-?>
